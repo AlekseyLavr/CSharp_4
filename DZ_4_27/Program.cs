adcc720e5ef8bb5f6117e1838d43b7b1a700cfe5
AlekseyLavr <govola77@gmail.com>
2023-07-19 21:05:53 +0300

    Домашнее задание № 27

---
+++ DZ_4_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int f(int n)
+{
+    int result = 0;
+    while (n > 0)
+    {
+        result += n % 10;
+        n /= 10;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма всех цифр в числе {n} равна {f(n)}");
