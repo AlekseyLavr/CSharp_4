3ad5912cca940a85b019c063895d5723c3f968ca
AlekseyLavr <govola77@gmail.com>
2023-07-19 21:50:04 +0300

    Домашнее задание № 29

---
+++ DZ_4_29/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write("]");
+}
+
+Console.Clear();
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите min: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите max: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] array = GenerateArray(length, min, max);
+PrintArray(array);
